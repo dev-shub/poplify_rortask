c2015d54966152b43a7cbccd5d7be37eefcb2eaa
Shubham Jain <shubham.jain@pristyncare.com>
2019-08-13 17:52:38 +0530

    Controllers and devise files

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'home#index'
+  devise_for :users
+  resources :users, only: [:show, :edit, :update]
+  resources :posts, only: [:new, :create, :show, :destroy] do
+    resources :likes
+    resources :comments
+  end
+  resources :comments do
+    resources :comments
+  end
+  get 'search' => 'search#index'
 end
